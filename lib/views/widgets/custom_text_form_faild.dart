6e9658a8954effc97dbcfc5f49ccfccc10b9df96
Mahmoud <mellebda@student.eelu.edu.eg>
2023-07-12 15:16:33 +0200

    create note cubit and create fetchAllNote done

--- lib/views/widgets/custom_text_form_faild.dart
+++ lib/views/widgets/custom_text_form_faild.dart
@@ -5,21 +5,23 @@ class CustomTextFormFaild extends StatelessWidget {
       {super.key,
       this.validator,
       required this.textEditingController,
-      required this.hintText,  this.maxLines});
+      required this.hintText,
+      this.maxLines});
   final TextEditingController textEditingController;
   final String? Function(String?)? validator;
   final String hintText;
-  final int? maxLines ;
+  final int? maxLines;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       maxLines: maxLines,
       decoration: InputDecoration(
-        hintText: hintText,
-        border: buildBorder(),
-        enabledBorder: buildBorder(),
-        disabledBorder: buildBorder()
-      ),
+          hintText: hintText,
+         hintTextDirection: TextDirection.rtl,
+       labelText: "sdsd",
+          border: buildBorder(),
+          enabledBorder: buildBorder(),
+          disabledBorder: buildBorder()),
       validator: validator,
       controller: textEditingController,
     );
